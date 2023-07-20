4bfe2a3d8efda6b753fb140638aa7ab9c52f0335
PaaaulZ <46759927+PaaaulZ@users.noreply.github.com>
2023-07-20 04:49:49 +0200

    Removed old method and ported SigScanner to .net to merge

--- PhotoViewerPatcher/Form1.Designer.vb
+++ PhotoViewerPatcher/Form1.Designer.vb
@@ -23,36 +23,23 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.btnBrowse = New System.Windows.Forms.Button()
-        Me.btnPatch = New System.Windows.Forms.Button()
         Me.txtPath = New System.Windows.Forms.TextBox()
         Me.lblPath = New System.Windows.Forms.Label()
-        Me.btnPatchSigScan = New System.Windows.Forms.Button()
-        Me.txtPathSigScanner = New System.Windows.Forms.TextBox()
-        Me.lblPathSig = New System.Windows.Forms.Label()
-        Me.btnBrowseSig = New System.Windows.Forms.Button()
+        Me.btnPatch = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'btnBrowse
         '
-        Me.btnBrowse.Location = New System.Drawing.Point(531, 69)
+        Me.btnBrowse.Location = New System.Drawing.Point(531, 24)
         Me.btnBrowse.Name = "btnBrowse"
         Me.btnBrowse.Size = New System.Drawing.Size(55, 23)
         Me.btnBrowse.TabIndex = 0
         Me.btnBrowse.Text = "Browse"
         Me.btnBrowse.UseVisualStyleBackColor = True
         '
-        'btnPatch
-        '
-        Me.btnPatch.Location = New System.Drawing.Point(495, 200)
-        Me.btnPatch.Name = "btnPatch"
-        Me.btnPatch.Size = New System.Drawing.Size(114, 69)
-        Me.btnPatch.TabIndex = 0
-        Me.btnPatch.Text = "Patch"
-        Me.btnPatch.UseVisualStyleBackColor = True
-        '
         'txtPath
         '
-        Me.txtPath.Location = New System.Drawing.Point(149, 71)
+        Me.txtPath.Location = New System.Drawing.Point(149, 26)
         Me.txtPath.Name = "txtPath"
         Me.txtPath.Size = New System.Drawing.Size(376, 20)
         Me.txtPath.TabIndex = 1
@@ -60,58 +47,29 @@ Partial Class Form1
         'lblPath
         '
         Me.lblPath.AutoSize = True
-        Me.lblPath.Location = New System.Drawing.Point(13, 74)
+        Me.lblPath.Location = New System.Drawing.Point(13, 29)
         Me.lblPath.Name = "lblPath"
         Me.lblPath.Size = New System.Drawing.Size(130, 13)
         Me.lblPath.TabIndex = 2
         Me.lblPath.Text = "Path to ImagingEngine.dll:"
         '
-        'btnPatchSigScan
+        'btnPatch
         '
-        Me.btnPatchSigScan.Location = New System.Drawing.Point(495, 275)
-        Me.btnPatchSigScan.Name = "btnPatchSigScan"
-        Me.btnPatchSigScan.Size = New System.Drawing.Size(114, 69)
-        Me.btnPatchSigScan.TabIndex = 3
-        Me.btnPatchSigScan.Text = "Patch (sigscan)"
-        Me.btnPatchSigScan.UseVisualStyleBackColor = True
-        '
-        'txtPathSigScanner
-        '
-        Me.txtPathSigScanner.Location = New System.Drawing.Point(149, 94)
-        Me.txtPathSigScanner.Name = "txtPathSigScanner"
-        Me.txtPathSigScanner.Size = New System.Drawing.Size(376, 20)
-        Me.txtPathSigScanner.TabIndex = 1
-        '
-        'lblPathSig
-        '
-        Me.lblPathSig.AutoSize = True
-        Me.lblPathSig.Location = New System.Drawing.Point(21, 97)
-        Me.lblPathSig.Name = "lblPathSig"
-        Me.lblPathSig.Size = New System.Drawing.Size(122, 13)
-        Me.lblPathSig.TabIndex = 2
-        Me.lblPathSig.Text = "Path to SigScanner.exe:"
-        '
-        'btnBrowseSig
-        '
-        Me.btnBrowseSig.Location = New System.Drawing.Point(531, 94)
-        Me.btnBrowseSig.Name = "btnBrowseSig"
-        Me.btnBrowseSig.Size = New System.Drawing.Size(55, 23)
-        Me.btnBrowseSig.TabIndex = 0
-        Me.btnBrowseSig.Text = "Browse"
-        Me.btnBrowseSig.UseVisualStyleBackColor = True
+        Me.btnPatch.Location = New System.Drawing.Point(472, 65)
+        Me.btnPatch.Name = "btnPatch"
+        Me.btnPatch.Size = New System.Drawing.Size(114, 69)
+        Me.btnPatch.TabIndex = 0
+        Me.btnPatch.Text = "Patch"
+        Me.btnPatch.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(621, 473)
-        Me.Controls.Add(Me.btnPatchSigScan)
-        Me.Controls.Add(Me.lblPathSig)
+        Me.ClientSize = New System.Drawing.Size(621, 157)
         Me.Controls.Add(Me.lblPath)
-        Me.Controls.Add(Me.txtPathSigScanner)
         Me.Controls.Add(Me.txtPath)
         Me.Controls.Add(Me.btnPatch)
-        Me.Controls.Add(Me.btnBrowseSig)
         Me.Controls.Add(Me.btnBrowse)
         Me.Name = "Form1"
         Me.Text = "Windows Photo Viewer | Patcher"
@@ -121,11 +79,7 @@ Partial Class Form1
     End Sub
 
     Friend WithEvents btnBrowse As Button
-    Friend WithEvents btnPatch As Button
     Friend WithEvents txtPath As TextBox
     Friend WithEvents lblPath As Label
-    Friend WithEvents btnPatchSigScan As Button
-    Friend WithEvents txtPathSigScanner As TextBox
-    Friend WithEvents lblPathSig As Label
-    Friend WithEvents btnBrowseSig As Button
+    Friend WithEvents btnPatch As Button
 End Class
--- PhotoViewerPatcher/Form1.vb
+++ PhotoViewerPatcher/Form1.vb
@@ -1,112 +1,30 @@
-﻿Imports System.Security.Cryptography
-Imports System.Text
-Imports System.IO
+﻿Imports System.IO
 
 Public Class Form1
 
-    Private VALID_CRC As New Dictionary(Of String, Integer)
+    Private Const PATTERN As String = "85 C0 75 ? ? 0D 03 15 86"
 
     Private Sub CreateBackup(ByVal path As String)
-
-        Dim pathOnly As String = System.IO.Path.GetDirectoryName(path)
-
-        File.Delete(pathOnly + "\ImagingEngine.dll.bak")
-        File.Copy(path, pathOnly + "\ImagingEngine.dll.bak")
-
+        File.Copy(path, $"{System.IO.Path.GetDirectoryName(path)}\ImagingEngine.dll.bak", True)
     End Sub
 
     Private Sub PatchFile(ByVal path As String, ByVal offset As Integer)
 
         CreateBackup(path)
 
-        Dim sr As FileStream = New FileStream(path, FileMode.Open, FileAccess.ReadWrite)
-        sr.Seek(offset, SeekOrigin.Begin)
+        Using fs As New FileStream(path, FileMode.Open, FileAccess.ReadWrite)
+            fs.Seek(offset, SeekOrigin.Begin)
 
-        Dim currentByte As Byte = sr.ReadByte()
-        If currentByte = &H75 Then
-            sr.Seek(offset, SeekOrigin.Begin)
-            sr.WriteByte(&HEB)
-        Else
-            MsgBox("Invalid byte at offset, unsupported dll version?")
-            Environment.Exit(1)
-        End If
-        sr.Close()
-
-    End Sub
-
-    ' --- INIT ---
-
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        ' Init dictionaries
-
-        ' --- Windows 10 Pro Version 1903 Build 18362.356 ---
-
-        ' x86 
-        VALID_CRC.Add("3F43D403517A4889856CFFAC7AB579545E9FFE0F022F3BF0262890EC4EC269EF", &HC93DE)
-        ' x64
-        VALID_CRC.Add("A532AE68CCC53DFDC190BB447AE9175A86C072DD99A374151F1D23EDABFCC1A2", &H9E56D)
-
-        ' --- Windows 10 Version 22H2 Build 19045.2486 ---
-
-        ' x86 
-        VALID_CRC.Add("1160360F25D5D1E263C7EADF03240B251B1DCB4263CB2AFCF431619A08346D0E", &HC88AD)
-        ' x64
-        VALID_CRC.Add("994476041A9AF3E546706B016A8890B7830FD94FF9C766DD18FF36530E456987", &HA07DC)
-
-        ' --- Windows 7 Enterprise Version 6.1.7601 Service Pack 1 Build 7601 ---
-
-        ' x86
-        VALID_CRC.Add("F732F0530A7FFD6C67FF774760174B8E01377DC1085942215870C85235F11C61", &HCAA9E)
-
-        ' --- Windows 11 Pro 10.0.22000 build 22000 ---
-
-        ' x64
-        VALID_CRC.Add("5176E7C7AD8C8C62D4B78C3300067758DA380EF85CED7DF150B50BB4DD981A84", &HC4D17)
-        ' x86
-        VALID_CRC.Add("806BD2FF9DDBE08E1E230FE4CA048312CEB88ACF0EEEDF3597938014FA496D48", &HCCAA0)
-
-
-
-    End Sub
-
-    ' --- UTILS ---
-    Private Function BytesToHexString(ByVal bytes_Input As Byte()) As String
-
-        ' https://social.msdn.microsoft.com/Forums/vstudio/en-US/fa53ce74-fd53-4d2a-bc05-619Fb9d32481/convert-Byte-array-To-hex-String?forum=vbgeneral
-
-        Dim strTemp As New StringBuilder(bytes_Input.Length * 2)
-
-        For Each b As Byte In bytes_Input
-
-            ' HACK: My calculated hash has zeros in front of single digits
-            Dim hexTMP As String = Conversion.Hex(b)
-            If hexTMP.Length = 1 Then
-                hexTMP = "0" + hexTMP
+            If fs.ReadByte() = &H75 Then
+                fs.Seek(offset, SeekOrigin.Begin)
+                fs.WriteByte(&HEB)
+            Else
+                MsgBox("Invalid byte at offset, unsupported dll version?")
+                Environment.Exit(1)
             End If
+        End Using
 
-            strTemp.Append(hexTMP)
-        Next
-
-        Return strTemp.ToString()
-
-    End Function
-
-    Private Function GetOffset(ByVal path As String)
-
-        Dim sha256 As SHA256 = SHA256Managed.Create()
-        Dim fileStream As Stream = New StreamReader(path).BaseStream
-        Dim hash As Byte() = sha256.ComputeHash(fileStream)
-        Dim hexString As String = BytesToHexString(hash)
-
-        fileStream.Close()
-
-        If VALID_CRC.Keys.Contains(hexString) Then
-            Return VALID_CRC(hexString)
-        Else
-            Return Nothing
-        End If
-
-    End Function
+    End Sub
 
     ' --- EVENTS ---
 
@@ -131,56 +49,45 @@ Public Class Form1
             pathToFile = txtPath.Text
         End If
 
-        Dim offset As Integer = GetOffset(pathToFile)
-        If IsNothing(offset) Then
-            If MessageBox.Show("Unknown ImagingEngine.dll version, do you want to try anyway?", "", MessageBoxButtons.YesNo, MessageBoxIcon.Question) = DialogResult.No Then
-                Environment.Exit(1)
-            Else
-                PatchFile(pathToFile, offset)
+        Dim j As Integer, firstFound As Integer = 0
+        Dim patternSplit As String() = PATTERN.Split(" ")
+
+        Dim fileBytes As Byte() = File.ReadAllBytes(pathToFile)
+        For i = 0 To fileBytes.Count - 1 Step 1
+
+            If patternSplit(j) = "?" Then
+                j += 1
+                If firstFound = 0 Then
+                    firstFound = i
+                End If
+                Continue For
             End If
-        Else
-            PatchFile(pathToFile, offset)
-        End If
 
-        MsgBox("Done.")
+            If fileBytes(i) = CByte($"&H{patternSplit(j)}") Then
+                j += 1
+                If firstFound = 0 Then
+                    firstFound = i
+                End If
+            Else
+                firstFound = 0
+                j = 0
+            End If
+
+            If j >= patternSplit.Count Then
+                Exit For
+            End If
+        Next
+
+        If firstFound > 0 Then
+            PatchFile(pathToFile, (firstFound + &H2))
+            MsgBox("Done")
+        Else
+            MsgBox("Could not find byte to patch. Already patched?")
+            Return
+        End If
 
     End Sub
 
-    Private Sub btnPatchSigScan_Click(sender As Object, e As EventArgs) Handles btnPatchSigScan.Click
-
-        Dim pathToFile As String = ""
-        Dim pathToFileSig As String = ""
-
-        If txtPathSigScanner.Text = "" Then
-            MsgBox("Please insert path for SigScanner.exe or Browse. Usually under ""Program files\Windows PhotoViewer""")
-            Environment.Exit(1)
-        Else
-            pathToFileSig = txtPathSigScanner.Text
-        End If
-
-        If txtPath.Text = "" Then
-            MsgBox("Please insert path for ImagingEngine.dll or Browse. Usually under ""Program files\Windows PhotoViewer""")
-            Environment.Exit(1)
-        Else
-            pathToFile = txtPath.Text
-        End If
-
-        Dim p As New Process()
-        p.StartInfo.FileName = pathToFileSig
-        p.StartInfo.Arguments = pathToFile + " " + pathToFile.Replace("ImagingEngine.dll", "ImagingEngine.dll.bak")
-        p.Start()
-
-    End Sub
-
-    Private Sub btnBrowseSig_Click(sender As Object, e As EventArgs) Handles btnBrowseSig.Click
-
-        Dim a As New OpenFileDialog() With {.Filter = "SigScanner executable|SigScanner.exe"}
-
-        If a.ShowDialog() = DialogResult.OK Then
-            txtPathSigScanner.Text = a.FileName
-        End If
-
-    End Sub
 End Class
 
 
